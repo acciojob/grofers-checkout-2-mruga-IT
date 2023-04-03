1628355b1299bd306af510f3ca5e6515182478fe
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-04-03 18:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+let totalPrice = 0;
+
+// loop through all prices and add them up
+for (let i = 0; i < prices.length; i++) {
+  totalPrice += parseFloat(prices[i].textContent);
+}
+
+// create new row for total price and add to table
+const table = document.getElementById('grocery-list');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+newCell.colSpan = 2;
+newCell.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+
+// set data attribute for total price row
+newRow.setAttribute('data-ns-test', 'grandTotal');
